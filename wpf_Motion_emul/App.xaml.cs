fc11eca349a1b091d14d2046932ab4daeda86108
Dmitry Egorov <freeze147yandex@gmail.com>
2016-10-14 15:41:49 +0300

    Добавьте файлы проекта.

---
+++ wpf_Motion_emul/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace wpf_Motion_emul {
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application {
+    }
+}
